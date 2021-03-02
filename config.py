f4727ee843b135938bf0a700964b7ddc6735c0fd
rajaganapathy2000 <79167443+rajaganapathy2000@users.noreply.github.com>
2021-03-03 01:40:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 TOKEN = os.environ.get("BOT_TOKEN")
 API_HASH = os.environ.get("API_HASH")
 API_ID = int(os.environ.get("API_ID"))
-START_MESSAGE = os.environ.get("START_MESSAGE", "<b>Hi ! I am a simple torrent searcher using @sjprojects's Torrent Searcher api.\n\n\nMade with 🐍 by @KeralasBots</b>")
-FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "<b>Made with ❤️ by @KeralasBots</b>")
+START_MESSAGE = os.environ.get("START_MESSAGE", "<b>Hi ! I am a simple torrent searcher using @sjprojects's Torrent Searcher api.\n\n\nMade with ❤️ by @SeriesDiscussionForum</b>")
+FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "<b>Made with ❤️ by @SeriesDiscussionForum</b>")
 TORRENTS = {}
